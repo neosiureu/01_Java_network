22a048a7ad8cc68417ce954f24ffc735634f3d1e
neosiureu <neosiureu@gmail.com>
2025-03-08 20:02:55 +0900

    학원에서 변경한 내용

--- 13_network_client/src/edu/kh/network/client/model/service/ClientService.java
+++ 13_network_client/src/edu/kh/network/client/model/service/ClientService.java
@@ -1,6 +1,13 @@
 package edu.kh.network.client.model.service;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientService {
 	
@@ -15,14 +22,78 @@ public class ClientService {
 		
 		int port = 8500;
 		
+		Socket clientSocket = null;
+		 
+		InputStream is =null;
+		OutputStream os = null;
 		
+		BufferedReader br = null;
+		PrintWriter pw = null;
 		
+		try {
+			System.out.print("client 입니다. ");
+			clientSocket = new Socket(serverIP, port);
+			
+			// 2) 서버와의 입출력 스트림 오픈
+			
+			if(clientSocket !=null) {
+				// 만약 서버에 연결이 성공 했으면
+				
+				is= clientSocket.getInputStream();
+				os= clientSocket.getOutputStream();
+				// 3) 보조 스트림을 통해 성능을 개선한다
+				br = new BufferedReader(new InputStreamReader(is));
+				pw = new PrintWriter(os);
+				
+				
+				// 4) 스트림을 통해 읽고 쓴다
+				// 서버에서 클라이언트에게 보낸 날짜 메시지를 한 줄 읽어와 콘솔창에 출력
+				String serverMessage = br.readLine();
+				System.out.println(serverMessage);
+				
+				// 이제 클라이언트가 메시지를 보내는 과정
+				
+				// 클라이언트에서 서버로 메시지 전송
+				
+				Scanner sc = new Scanner(System.in);
+				System.out.println("서버로 전달할 메시지: ");
+				String str = sc.nextLine();
+				
+				pw.println(str); // 위에서 입력한 str에 담긴 메시지를 클라이언트에서 서버쪽으로 출력
+				pw.flush();							
+			}
+			
 		
+			
+		}
+		
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		finally {
+			// 5) 통신 종료
+
+			// 스트림과 소켓을 종료한다
+			
+			
+			try {
+				if(br!=null) br.close();
+				if(pw!=null) pw.close();
+				if(clientSocket!=null) clientSocket.close();
+				
+
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			
+		}
 		// 1) 서버의 IP주소와 서버가 정한 포트번호를 매개변수로 하여 클라이언트용 소켓 객체 생성
 		// 2) 서버와의 입출력 스트림 오픈
 		// 3) 보조 스트림을 통해 성능 개선
 		// 4) 스트림을 통해 읽고 쓰기
-		// 5) 통신 종료
 		
 		
 		
